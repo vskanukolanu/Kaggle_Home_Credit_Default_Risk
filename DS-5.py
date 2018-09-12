e8f8677f4a14b4004af713b98ac4100a77f482a8
Venkata Kanukolanu <33648934+vskanukolanu@users.noreply.github.com>
2018-09-12 00:08:48 -0700

    Add files via upload

---
+++ DS-5.py
@@ -0,0 +1,358 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+from IPython import get_ipython
+get_ipython().magic('reset -sf')
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# In[2]:
+
+
+from sklearn.metrics import confusion_matrix
+import itertools
+import pandas as pd
+import numpy as np
+import xgboost as xgb
+from xgboost.sklearn import XGBClassifier
+from sklearn import cross_validation, metrics   #Additional scklearn functions
+from sklearn.grid_search import GridSearchCV   #Perforing grid search
+#from imblearn.over_sampling import SMOTE       #over sampling of minority class in imbalanced data
+#from imblearn.combine import SMOTEENN          #over sampling of minority class in imbalanced data
+from sklearn.model_selection import train_test_split, cross_validate, StratifiedKFold,GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn import preprocessing, model_selection, metrics
+import lightgbm as lgb
+import gc
+from sklearn.metrics import roc_auc_score
+from tqdm import tqdm_notebook as tqdm
+from scipy.stats import skew, kurtosis, iqr
+
+from functools import partial
+from sklearn.externals import joblib
+
+from sklearn.feature_selection import RFE,RFECV
+from sklearn.linear_model import LogisticRegression,LogisticRegressionCV
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import recall_score,average_precision_score,precision_recall_curve,precision_score
+from functools import partial,reduce
+import seaborn as sns
+import matplotlib.pylab as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+pd.set_option('display.max_columns', 50)
+pd.set_option('display.max_columns', 100)
+
+
+# In[3]:
+
+
+import logging
+import os
+import random
+import sys
+import multiprocessing as mp
+from functools import reduce
+
+import glob
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+#import yaml
+from attrdict import AttrDict
+
+
+# In[4]:
+
+
+# read the input files and look at the top few lines #
+data_path = "/Users/venkatasravankanukolanu/Documents/Data Files/home_credit_kaggle/"
+app_fe2_df= pd.read_csv(data_path+"app_df_v5.csv")
+apptest_fe2_df= pd.read_csv(data_path+"app_test_v5.csv")
+
+
+# In[5]:
+
+
+# read the input files and look at the top few lines #
+data_path = "/Users/venkatasravankanukolanu/Documents/Data Files/home_credit_kaggle/"
+inst_payments= pd.read_csv(data_path+"installments_payments.csv")
+inst_payments.head(2)
+
+
+# In[6]:
+
+
+### Difference in instalment payment to actual payment made
+inst_payments['diff_payment']=(inst_payments['AMT_PAYMENT']-inst_payments['AMT_INSTALMENT'])
+
+### Difference in instalment payment date to actual payment date
+inst_payments['diff_days']=((-1*inst_payments['DAYS_ENTRY_PAYMENT'])-(-1*inst_payments['DAYS_INSTALMENT']))
+
+inst_payments['instalment_paid_late'] = (inst_payments['diff_days'] > 0).astype(int)
+inst_payments['instalment_paid_over'] = (inst_payments['diff_payment'] > 0).astype(int)
+
+
+# In[7]:
+
+
+INSTALLMENTS_PAYMENTS_AGGREGATION_RECIPIES = []
+for agg in ['mean', 'min', 'max', 'sum', 'var']:
+    for select in ['AMT_INSTALMENT',
+                   'AMT_PAYMENT',
+                   'DAYS_ENTRY_PAYMENT',
+                   'DAYS_INSTALMENT',
+                   'NUM_INSTALMENT_NUMBER',
+                   'NUM_INSTALMENT_VERSION',
+                   'diff_payment',
+                   'diff_days'
+                   ]:
+        INSTALLMENTS_PAYMENTS_AGGREGATION_RECIPIES.append((select, agg))
+INSTALLMENTS_PAYMENTS_AGGREGATION_RECIPIES = [(['SK_ID_CURR'], INSTALLMENTS_PAYMENTS_AGGREGATION_RECIPIES)]
+
+
+# In[8]:
+
+
+groupby_aggregate_names = []
+for groupby_cols, specs in tqdm(INSTALLMENTS_PAYMENTS_AGGREGATION_RECIPIES):
+    group_object = inst_payments.groupby(groupby_cols)
+    for select, agg in tqdm(specs):
+        groupby_aggregate_name = '{}_{}_{}'.format('_'.join(groupby_cols), agg, select)
+        app_fe2_df = app_fe2_df.merge(group_object[select]
+                              .agg(agg)
+                              .reset_index()
+                              .rename(index=str,
+                                      columns={select: groupby_aggregate_name})
+                              [groupby_cols + [groupby_aggregate_name]],
+                              on=groupby_cols,
+                              how='left')
+        groupby_aggregate_names.append(groupby_aggregate_name)
+
+
+# In[9]:
+
+
+groupby_aggregate_names = []
+for groupby_cols, specs in tqdm(INSTALLMENTS_PAYMENTS_AGGREGATION_RECIPIES):
+    group_object = inst_payments.groupby(groupby_cols)
+    for select, agg in tqdm(specs):
+        groupby_aggregate_name = '{}_{}_{}'.format('_'.join(groupby_cols), agg, select)
+        apptest_fe2_df = apptest_fe2_df.merge(group_object[select]
+                              .agg(agg)
+                              .reset_index()
+                              .rename(index=str,
+                                      columns={select: groupby_aggregate_name})
+                              [groupby_cols + [groupby_aggregate_name]],
+                              on=groupby_cols,
+                              how='left')
+        groupby_aggregate_names.append(groupby_aggregate_name)
+
+
+# In[10]:
+
+
+apptest_fe2_df.to_csv(data_path+"app_test_v6.csv", index = False)
+
+
+# In[11]:
+
+
+app_fe2_df.to_csv(data_path+"app_df_v6.csv", index = False)
+
+
+# #### Aggregates from last k instalments
+
+# In[12]:
+
+
+def parallel_apply(groups, func, index_name='Index', num_workers=1, chunk_size=1000):
+    n_chunks = np.ceil(1.0 * groups.ngroups / chunk_size)
+    indeces, features  = [],[]
+    for index_chunk, groups_chunk in tqdm(chunk_groups(groups, chunk_size), total=n_chunks):
+        with mp.pool.Pool(num_workers) as executor:
+            features_chunk = executor.map(func, groups_chunk)
+        features.extend(features_chunk)
+        indeces.extend(index_chunk)
+    features = pd.DataFrame(features)
+    features.index = indeces
+    features.index.name = index_name
+    return features
+
+def chunk_groups(groupby_object, chunk_size):
+    n_groups = groupby_object.ngroups
+    group_chunk, index_chunk = [],[]
+    for i, (index, df) in enumerate(groupby_object):
+        group_chunk.append(df)
+        index_chunk.append(index)
+
+        if (i + 1) % chunk_size == 0 or i + 1 == n_groups:
+            group_chunk_, index_chunk_ = group_chunk.copy(), index_chunk.copy()
+            group_chunk, index_chunk = [],[]
+            yield index_chunk_, group_chunk_
+
+
+# In[13]:
+
+
+def add_features(feature_name, aggs, features, feature_names, groupby):
+    feature_names.extend(['{}_{}'.format(feature_name, agg) for agg in aggs])
+
+    for agg in aggs:
+        if agg == 'kurt':
+            agg_func = kurtosis
+        elif agg == 'iqr':
+            agg_func = iqr
+        else:
+            agg_func = agg
+        
+        g = groupby[feature_name].agg(agg_func).reset_index().rename(index=str,
+                                                                columns={feature_name: '{}_{}'.format(feature_name,
+                                                                                                      agg)})
+        features = features.merge(g, on='SK_ID_CURR', how='left')
+    return features, feature_names
+
+
+def add_features_in_group(features, gr_, feature_name, aggs, prefix):
+    for agg in aggs:
+        if agg == 'sum':
+            features['{}{}_sum'.format(prefix, feature_name)] = gr_[feature_name].sum()
+        elif agg == 'mean':
+            features['{}{}_mean'.format(prefix, feature_name)] = gr_[feature_name].mean()
+        elif agg == 'max':
+            features['{}{}_max'.format(prefix, feature_name)] = gr_[feature_name].max()
+        elif agg == 'min':
+            features['{}{}_min'.format(prefix, feature_name)] = gr_[feature_name].min()
+        elif agg == 'std':
+            features['{}{}_std'.format(prefix, feature_name)] = gr_[feature_name].std()
+        elif agg == 'count':
+            features['{}{}_count'.format(prefix, feature_name)] = gr_[feature_name].count()
+        elif agg == 'skew':
+            features['{}{}_skew'.format(prefix, feature_name)] = skew(gr_[feature_name])
+        elif agg == 'kurt':
+            features['{}{}_kurt'.format(prefix, feature_name)] = kurtosis(gr_[feature_name])
+        elif agg == 'iqr':
+            features['{}{}_iqr'.format(prefix, feature_name)] = iqr(gr_[feature_name])
+        elif agg == 'median':
+            features['{}{}_median'.format(prefix, feature_name)] = gr_[feature_name].median()
+    return features
+
+
+# In[15]:
+
+
+features = pd.DataFrame({'SK_ID_CURR':inst_payments['SK_ID_CURR'].unique()})
+groupby = inst_payments.groupby(['SK_ID_CURR'])
+
+
+# In[16]:
+
+
+feature_names = []
+
+features, feature_names = add_features('NUM_INSTALMENT_VERSION', 
+                                       ['sum','mean','max','min','std', 'median','skew', 'kurt','iqr'],
+                                     features, feature_names, groupby)
+
+features, feature_names = add_features('diff_days', 
+                                       ['sum','mean','max','min','std', 'median','skew', 'kurt','iqr'],
+                                     features, feature_names, groupby)
+
+features, feature_names = add_features('instalment_paid_late', ['sum','mean'],
+                                     features, feature_names, groupby)
+
+features, feature_names = add_features('diff_payment', 
+                                       ['sum','mean','max','min','std', 'median','skew', 'kurt','iqr'],
+                                     features, feature_names, groupby)
+
+features, feature_names = add_features('instalment_paid_over', ['sum','mean'],
+                                     features, feature_names, groupby)
+
+
+# In[17]:
+
+
+features.shape
+
+
+# In[18]:
+
+
+def last_k_instalment_features(gr,periods):
+    gr_ = gr
+    gr_.sort_values(['DAYS_INSTALMENT'],ascending=False, inplace=True)
+    
+    features = {}
+
+    for period in periods:
+        gr_period = gr_.iloc[:period]
+
+        features = add_features_in_group(features,gr_period, 'NUM_INSTALMENT_VERSION', 
+                                       ['sum','mean','max','min','std', 'median','skew', 'kurt','iqr'],
+                                         'last_{}_'.format(period))
+        
+        features = add_features_in_group(features,gr_period, 'diff_days', 
+                                       ['sum','mean','max','min','std', 'median','skew', 'kurt','iqr'],
+                                         'last_{}_'.format(period))
+        features = add_features_in_group(features,gr_period ,'instalment_paid_late', 
+                                     ['count','mean'],
+                                         'last_{}_'.format(period))
+        features = add_features_in_group(features,gr_period ,'diff_payment', 
+                                       ['sum','mean','max','min','std', 'median','skew', 'kurt','iqr'],
+                                         'last_{}_'.format(period))
+        features = add_features_in_group(features,gr_period,'instalment_paid_over', 
+                                     ['count','mean'],
+                                         'last_{}_'.format(period))        
+    
+    return features
+
+
+# In[19]:
+
+
+func = partial(last_k_instalment_features, periods=[1,5,10,20,50,100])
+
+g = parallel_apply(groupby, func, index_name='SK_ID_CURR',
+                   num_workers=1, chunk_size=10000).reset_index()
+features = features.merge(g, on='SK_ID_CURR', how='left')
+
+display(features.head())
+
+
+# In[20]:
+
+
+app_fe2_df = app_fe2_df.merge(features, on='SK_ID_CURR', how='left')
+apptest_fe2_df = apptest_fe2_df.merge(features, on='SK_ID_CURR', how='left')
+
+
+# In[21]:
+
+
+apptest_fe2_df.to_csv(data_path+"app_test_v7.csv", index = False)
+
+
+# In[22]:
+
+
+app_fe2_df.to_csv(data_path+"app_df_v7.csv", index = False)
+
+
+# In[23]:
+
+
+app_fe2_df.shape
+
+
+# In[24]:
+
+
+apptest_fe2_df.shape
+
